218cbdec1f14304f9691052e36a81152bf484176
Faizah Afifah <faizahafifah00@gmail.com>
2018-09-07 14:15:35 +0700

    edit setter getter

--- mahasiswa.java
+++ mahasiswa.java
@@ -1,10 +1,33 @@
 import *;
 
 public class Tester{
+	private String name;
+	prvate int age;
 	
 	public static void main (String[] args) {
 		System.out.println("Halo mahasiswa!");
 		
+	}
+	
+	public Mahasiswa(){
+		this.name = "faizah afifah";
+		this.age = 20;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getAge(){
+		return age;
 	}
 	
 }	
